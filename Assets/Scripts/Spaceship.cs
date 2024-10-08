20de9e409a6a9f31dccf9df12d9dad91fb438206
Katrina <1051942859@qq.com>
2024-10-09 00:27:10 +0800

    实现陀螺仪转地图

--- Assets/Scripts/Spaceship.cs
+++ Assets/Scripts/Spaceship.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Spaceship : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Rigidbody2D rb;
+    float dirX;
+    float moveSpeed = 20f;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        dirX = Input.acceleration.x * moveSpeed;
+        //transform.position = new Vector2(Mathf.Clamp(transform.position.x, -7.5f, 7.5f), transform.position.y);
+        //transform.eulerAngles = new Vector3(0, 0, Mathf.Clamp(transform.rotation.z, -90f, 90f));
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MoveRotation(dirX * 5f);
     }
 }
